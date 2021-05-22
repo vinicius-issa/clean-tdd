f9c80ebab125d5902ec4af05d3dae017ee51def9
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-05-22 12:31:20 -0300

    refactor: move duplicated code to a helper request

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,19 +1,14 @@
 import { MissignParamError } from '../errors/missing-params-error'
+import { badRequest } from '../helpers/http-helpers'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (httpRequest.body.name === undefined) {
-      return {
-        statusCode: 400,
-        body: new MissignParamError('name')
-      }
+      return badRequest(new MissignParamError('name'))
     }
     if (httpRequest.body.email === undefined) {
-      return {
-        statusCode: 400,
-        body: new MissignParamError('email')
-      }
+      return badRequest(new MissignParamError('email'))
     }
   }
 }
