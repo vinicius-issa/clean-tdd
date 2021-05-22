bcc6609ba7fccaac93d1217dba74e6a9a7e73bb4
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-05-22 12:42:49 -0300

    refactor: make SignController conforms to a protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { MissignParamError } from '../errors/missing-params-error'
 import { badRequest } from '../helpers/http-helpers'
+import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
