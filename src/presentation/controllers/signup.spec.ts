357ccbbbd2b20e116e54350fffcc1415fca36647
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-05-22 12:06:00 -0300

    test: add ensure SignuController return an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,5 +12,6 @@ describe('Signup Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missign param: name'))
   })
 })
