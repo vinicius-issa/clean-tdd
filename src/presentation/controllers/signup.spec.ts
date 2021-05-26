61d9ac5f1a488ffed02f270ba6e93c9bb6e3ea8a
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-05-25 22:38:32 -0300

    test: ensure SignUp Controller returns 400 if password confirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -65,7 +65,7 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new MissignParamError('password'))
   })
 
-  test('Show return 400 if no password is provided', () => {
+  test('Show return 400 if no password confirmation is provided', () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
@@ -77,6 +77,21 @@ describe('Signup Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissignParamError('passwordConfirmation'))
+  })
+
+  test('Show return 400 if password confirmation fails', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'wrong_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
   })
 
   test('Show return 400 if an invalid is provided', () => {
