b07e7cf7a60fc5bf080759a915db00c6a8fe58bb
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-05-27 17:47:02 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -3,17 +3,16 @@
  * https://jestjs.io/docs/en/configuration.html
  */
 
-export default {
+module.exports = {
 
   roots: ['<rootDir>/src'],
-
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
 
   // The test environment that will be used for testing
   testEnvironment: 'node',
