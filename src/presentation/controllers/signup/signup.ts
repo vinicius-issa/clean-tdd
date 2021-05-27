d5f271d4c003cec7b115e1c90d546d34e20eace2
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-05-27 16:45:34 -0300

    feat: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -37,9 +37,15 @@ export class SignUpController implements Controller {
         name
       })
 
+      const account = this.addAccount.add({
+        name,
+        email,
+        password
+      })
+
       return {
-        statusCode: 0,
-        body: ''
+        statusCode: 200,
+        body: account
       }
     } catch (error) {
       return serverError()
