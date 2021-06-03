3e5db91c4be5424076c440cdb8427a2eef69554d
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-06-03 17:49:26 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,10 @@
-import app from './config/app'
+/* eslint-disable promise/always-return */
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-app.listen(5000, () => console.log('Server running at http://localhost:5000'))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
