15718a915cecb5fb3beff101a037569b1b660a27
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-06-05 12:50:56 -0300

    test: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/return-await */
+import { Controller, HttpResponse } from '../../presentation/protocols'
+import { LogControllerDecorator } from './log'
+
+interface SutTypes {
+  sut: LogControllerDecorator,
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpResponse): Promise<HttpResponse> {
+      const httpReponse = {
+        statusCode: 200,
+        body: {
+          name: 'vinicius'
+        }
+      }
+      return new Promise(resolve => resolve(httpReponse))
+    }
+  }
+  return new ControllerStub()
+}
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+describe('Log Controller Decorator', () => {
+  test('Should call controller handle ', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
