4f2c10f0963789393437957bad4fea699fe9f752
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-06-07 22:24:31 -0300

    feat: ensure LoginController calls emailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -2,8 +2,15 @@
 import { MissignParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { EmailValidator } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.email) {
       return badRequest(new MissignParamError('email'))
@@ -11,6 +18,7 @@ export class LoginController implements Controller {
     if (!httpRequest.body.password) {
       return badRequest(new MissignParamError('password'))
     }
+    this.emailValidator.isValid(httpRequest.body.email)
     return {
       statusCode: 0,
       body: {}
