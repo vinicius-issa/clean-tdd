415f283cb097d8af290fef506a698f373b595eed
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-06-07 22:08:34 -0300

    test: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,16 @@
+import { LoginController } from './login'
+import { badRequest } from '../../helpers/http-helpers'
+import { MissignParamError } from '../../errors'
+
+describe('Login Controller', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const sut = new LoginController()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissignParamError('email')))
+  })
+})
