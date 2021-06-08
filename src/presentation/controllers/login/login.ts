86c0972b30a54daa55038640bf903fb075dba93c
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-06-07 22:36:09 -0300

    feat: ensure LoginController return 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { InvalidParamError, MissignParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helpers'
+import { badRequest, serverError } from '../../helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { EmailValidator } from '../signup/signup-protocols'
 
@@ -12,20 +12,24 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return badRequest(new MissignParamError('email'))
-    }
-    if (!password) {
-      return badRequest(new MissignParamError('password'))
-    }
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-    return {
-      statusCode: 0,
-      body: {}
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return badRequest(new MissignParamError('email'))
+      }
+      if (!password) {
+        return badRequest(new MissignParamError('password'))
+      }
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+      return {
+        statusCode: 0,
+        body: {}
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
