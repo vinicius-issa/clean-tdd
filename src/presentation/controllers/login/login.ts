2d7592719985802cd009cf2a8bf0ed2e79679682
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-06-07 22:13:28 -0300

    feat: ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,9 +1,19 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { MissignParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class LoginController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new MissignParamError('email'))
+    if (!httpRequest.body.email) {
+      return badRequest(new MissignParamError('email'))
+    }
+    if (!httpRequest.body.password) {
+      return badRequest(new MissignParamError('password'))
+    }
+    return {
+      statusCode: 0,
+      body: {}
+    }
   }
 }
