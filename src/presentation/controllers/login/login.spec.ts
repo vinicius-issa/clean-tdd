f975cc42ae77bebda6eee0a28ae689dad37a6960
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-06-07 23:18:42 -0300

    test: ensure LoginControllers return 200 if valid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,6 +1,7 @@
 import { LoginController } from './login'
 import {
   badRequest,
+  ok,
   serverError,
   unauthorized
 } from '../../helpers/http-helpers'
@@ -130,5 +131,17 @@ describe('Login Controller', () => {
     })
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 200 if valid credentias are provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = makeFakeRequest()
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(
+      ok({
+        accessToken: 'any_token'
+      })
+    )
   })
 })
