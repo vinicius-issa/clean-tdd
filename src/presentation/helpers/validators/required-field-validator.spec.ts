508e63154fa27cfa292f8c1c7132266f8188b6c7
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-06-28 19:55:52 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/required-field-validator.spec.ts
+++ src/presentation/helpers/validators/required-field-validator.spec.ts
@@ -1,15 +1,19 @@
 import { MissignParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
+
 describe('RequiredField Validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('any_field')
+    const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
-    expect(error).toEqual(new MissignParamError('any_field'))
+    expect(error).toEqual(new MissignParamError('field'))
   })
 
   test('Should return null if validation succeeds', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ field: 'field' })
     expect(error).toBeNull()
   })
