c07df4998b8f60869d5a74de553cd97b5bb3366b
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-06-28 20:13:46 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,15 +2,35 @@ import { MissignParamError } from '../../errors'
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
+interface SutTypes {
+  sut: ValidationComposite,
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | null {
+      return null
+    }
+  }
+  const validationStub = new ValidationStub()
+  const sut = new ValidationComposite([validationStub])
+  return {
+    sut,
+    validationStub
+  }
+}
+
 describe('ValidationComposite', () => {
   test('Should return an error if any validation fails', () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error {
-        return new MissignParamError('any_field')
-      }
-    }
-    const validationStub = new ValidationStub()
-    const sut = new ValidationComposite([validationStub])
+    const {
+      sut,
+      validationStub
+    } = makeSut()
+    jest.spyOn(
+      validationStub,
+      'validate'
+    ).mockReturnValueOnce(new MissignParamError('any_field'))
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new MissignParamError('any_field'))
   })
