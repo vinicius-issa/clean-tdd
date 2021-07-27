40ed32776eb9390eeae83f197204a10034fcfdf3
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-07-27 18:52:05 -0300

    test: ensure DbAuthentication return a valid access token

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -161,4 +161,12 @@ describe('DBAuthentication Usecase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return accessToken', async () => {
+    const {
+      sut
+    } = makeSut()
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBe('any_token')
+  })
 })
