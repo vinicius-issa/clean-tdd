e2481c8633704eb2cd1bd3794ba64f20ae847185
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-07-28 20:20:59 -0300

    refactor: move protocols to a single file

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,15 +1,13 @@
 /* eslint-disable max-len */
-import { AccountModel } from '../../../domain/models/account'
-import { AuthenticationModel } from '../../../domain/usecases/authentication'
-import { HashComparer } from '../../protocols/criptography/hash-comparer'
-import { TokenGenerator } from '../../protocols/criptography/token-generator'
-import {
-  LoadAccountByEmailRepository
-} from '../../protocols/db/load-account-by-email-repository'
-import {
-  UpdateAccessTokenRepository
-} from '../../protocols/db/update-access-token-repository'
 import { DbAuthentication } from './db-authentication'
+import {
+  AccountModel,
+  AuthenticationModel,
+  HashComparer,
+  TokenGenerator,
+  LoadAccountByEmailRepository,
+  UpdateAccessTokenRepository
+} from './db-authentication-protocols'
 
 interface SutTypes {
   sut: DbAuthentication
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,15 +1,12 @@
+
 import {
   Authentication,
-  AuthenticationModel
-} from '../../../domain/usecases/authentication'
-import { HashComparer } from '../../protocols/criptography/hash-comparer'
-import { TokenGenerator } from '../../protocols/criptography/token-generator'
-import {
-  LoadAccountByEmailRepository
-} from '../../protocols/db/load-account-by-email-repository'
-import {
+  AuthenticationModel,
+  HashComparer,
+  TokenGenerator,
+  LoadAccountByEmailRepository,
   UpdateAccessTokenRepository
-} from '../../protocols/db/update-access-token-repository'
+} from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
