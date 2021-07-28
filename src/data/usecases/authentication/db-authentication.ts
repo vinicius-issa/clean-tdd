c21be66b468335513e39e342b2999d157c97648c
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-07-28 20:13:08 -0300

    feat: ensure DbAuthenticaton calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -7,20 +7,26 @@ import { TokenGenerator } from '../../protocols/criptography/token-generator'
 import {
   LoadAccountByEmailRepository
 } from '../../protocols/db/load-account-by-email-repository'
+import {
+  UpdateAccessTokenRepository
+} from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
 
   constructor (
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashComparer: HashComparer,
-    tokenGenerator: TokenGenerator
+    tokenGenerator: TokenGenerator,
+    updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.updateAccessTokenRepository = updateAccessTokenRepository
   }
 
   async auth (authentication: AuthenticationModel): Promise<string|null> {
@@ -39,8 +45,12 @@ export class DbAuthentication implements Authentication {
     if (!isValid) {
       return null
     }
-
     const token = await this.tokenGenerator.generate(account.id)
+
+    await this.updateAccessTokenRepository.update(
+      account.id,
+      token
+    )
 
     return token
   }
