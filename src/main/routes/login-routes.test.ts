cfc6896755ec6c2de7ce9dd86ae1b623dbc0f0e0
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-08-19 20:49:50 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-describe('Signup Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
   })
@@ -16,15 +16,17 @@ describe('Signup Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Vinicius',
-        email: 'vinicius.almeidaissa@gmail.com',
-        password: '123456',
-        passwordConfirmation: '123456'
-      })
-      .expect(200)
+  describe('Login Routes', () => {
+    test('Should return an account on success', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Vinicius',
+          email: 'vinicius.almeidaissa@gmail.com',
+          password: '123456',
+          passwordConfirmation: '123456'
+        })
+        .expect(200)
+    })
   })
 })
