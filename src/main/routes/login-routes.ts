c46decade4ab51ba693f1dcca522fc13d323fcf3
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-08-19 21:21:03 -0300

    feat: add login route

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -2,6 +2,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapter/express/express-route-adapter'
 import { makeSignupController } from '../factories/signup/signup-factory'
+import { makeLoginController } from '../factories/login/login-factory'
+
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignupController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
