59a183e8f9e883b5cd9c76e3f3c6b23e3eaf4945
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-08-21 15:23:58 -0300

    chore: add engines with node 14,x

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb+srv://clean-user:myCleanUs1V@cluster0.v8ggt.mongodb.net/clean-code-api?retryWrites=true&w=majority',
   port: process.env.PORT ?? 5000,
   jwtSecret: process.env.JWT_SECRET ?? 'tj607+=d'
 }
