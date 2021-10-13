e657fa4fa451c6b4b53218af0538ae802bb35c3f
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-10-13 10:10:50 -0300

    feat: ensure AddSurveyController returns 500 if AddSurveyUseCase throws

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,4 +1,4 @@
-import { badRequest } from '../../../helpers/http/http-helpers'
+import { badRequest, serverError } from '../../../helpers/http/http-helpers'
 import { AddSurvey, Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
@@ -8,20 +8,24 @@ export class AddSurveyController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error !== null) {
-      return badRequest(error)
-    }
-    const {
-      question, answers
-    } = httpRequest.body
-    await this.addSurvey.add({
-      question,
-      answers
-    })
-    return {
-      statusCode: 900,
-      body: {}
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error !== null) {
+        return badRequest(error)
+      }
+      const {
+        question, answers
+      } = httpRequest.body
+      await this.addSurvey.add({
+        question,
+        answers
+      })
+      return {
+        statusCode: 900,
+        body: {}
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
