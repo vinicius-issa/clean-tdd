96a1966639965ee707dd04c625ea71fc08c1bbbb
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-10-14 10:03:30 -0300

    feat: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,9 +1,18 @@
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccessDeniedError } from '../errors'
 import { forbideen } from '../helpers/http/http-helpers'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
+  constructor (
+    private readonly loadAccountByToken: LoadAccountByToken
+  ) {}
+
   async handle (HttpRequest: HttpRequest): Promise<HttpResponse> {
+    const accessToken = HttpRequest.headers?.['x-access-token']
+    if (accessToken !== undefined) {
+      await this.loadAccountByToken.load(accessToken)
+    }
     return forbideen(new AccessDeniedError())
   }
 }
